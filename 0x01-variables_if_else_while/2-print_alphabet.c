e54d9f149fec9d0ba6f6c1d9eb41da65f8275eee
Emmoloks <emmokaloki77@gmail,com>
2023-08-13 01:51:45 -0700

    Never new a-z could be this hard

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
