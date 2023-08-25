ff931fe34e66945b239d0198edf4f915bb324b7a
Emmoloks <emmokaloki77@gmail,com>
2023-08-25 06:17:44 -0700

    didnot know getting abs was this easy

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - it calculates the absolute value of an integer
+ * @i: The integer to calculate its absolute
+ * Return: absolute value
+ */
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		return (-i);
+	}
+	else
+	{
+		return (i);
+	}
+}
