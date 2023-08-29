685057873f771957614de94042d2777277e958b7
Emmoloks <emmokaloki77@gmail,com>
2023-08-29 02:26:19 -0700

    printing numbers from 0-9 except 2 & 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers -prints 0-9 expect 2 & 4
+ */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i != 2 && i != 4)
+		_putchar('0' + i);
+		i++;
+	}
+	_putchar('\n');
+}
