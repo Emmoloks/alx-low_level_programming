9e1afd41295a1df26435c3cf16c36ac8a6f3d97a
Emmoloks <emmokaloki77@gmail.com>
2023-09-06 03:25:46 -0700

    every other wasant digging it

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - functions that prints every other character
+ * @str: parameter to be taken
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
