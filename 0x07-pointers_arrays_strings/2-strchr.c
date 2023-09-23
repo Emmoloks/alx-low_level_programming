abd7e54469f2a211227ff2d0848345fdf78e55ef
Emmoloks <emmokaloki77@gmail.com>
2023-09-23 01:32:52 -0700

    string char

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strchr - locates character in a string
+ *@s: string to search in
+ *@c: character to trace
+ *Return: pointer to character or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	while (i >= 0)
+	{
+		if (s[i] == c)
+			return (s + i);
+		i--;
+	}
+	return ('\0');
+}
