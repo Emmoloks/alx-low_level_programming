8488892db3afd9120e35cb06a331d4b0f3b45b93
Emmoloks <emmokaloki77@gmail.com>
2023-09-27 12:21:26 -0700

    onana thats my name

---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+my work on functions and pointers
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,15 @@
+#ifndef FUNCTION_POINTERS_HEADER
+#define FUNCTION_POINTERS_HEADER
+void print_name(char *name, void (*f)(char *));
+
+
+
+
+
+
+
+
+
+
+
+#endif
