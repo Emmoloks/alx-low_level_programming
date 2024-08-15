3c1d3f7c56fa875c3041b96fa27641e2635966b2
Emmoloks <emmanuelkaloki77@gmail.com>
2024-08-15 11:40:03 +0300

    redoing c tasks

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: The maximum number for the times table
+ *
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	int i, j;
+
+	if (n < 0 || n > 15)
+	{
+		return;
+	}
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			int result = i * j;
+
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (result < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + result);
+			}
+			else if (result < 100)
+			{
+				_putchar(' ');
+				_putchar('0' + (result / 10));
+				_putchar('0' + (result % 10));
+			}
+			else
+			{
+				_putchar('0' + (result / 100));
+				_putchar('0' + ((result / 10) % 10));
+				_putchar('0' + (result % 10));
+			}
+		}
+		_putchar('\n');
+	}
+}
